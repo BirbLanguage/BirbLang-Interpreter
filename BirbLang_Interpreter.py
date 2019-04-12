36713546e6863b396fbdd8987eb3a25b8953f3a1
BirbLanguage <45858169+BirbLanguage@users.noreply.github.com>
2019-04-11 20:55:49 -0500

    Added the ability to solve math equations

--- BirbLang_Interpreter.py
+++ BirbLang_Interpreter.py
@@ -6,13 +6,142 @@ class BirbLang:
     def __init__(self, program):
         self.program = program
 
-    def removeAll(self, array, character):
-        return [x for x in array if x != character]
+    # Defines a function which returns the number of occurrences of a specific character
+    @staticmethod
+    def count(arr, character):
+        number = 0
+        for x in arr:
+            if x == character:
+                number += 1
+            else:
+                continue
+        return number
+
+    # Defines a function which separates a string while keeping all delimiters
+    @staticmethod
+    def safesplit(string, character1=None, character2=None, character3=None):
+        temp = ""
+        arr = []
+        for x in string:
+            if x != character1 and x != character2 and x != character3:
+                temp += x
+
+            else:
+                arr.append(temp)
+                if x == character1:
+                    arr.append(character1)
+                elif x == character2:
+                    arr.append(character2)
+                else:
+                    arr.append(character3)
+                temp = ""
+        arr.append(temp)
+
+        return arr
+
+    # Defines a function which removes every occurrence of a string in an array
+    @staticmethod
+    def removeall(arr, character):
+        return [x for x in arr if x != character]
+
+    # Defines a function which returns the nth index of a specific character within an array
+    @staticmethod
+    def indexofoccurrence(arr, character, occurence):
+        return [x for x in range(0, len(arr)) if arr[x] == character][occurence - 1]
+
+    # Calculates a math equation
+    @staticmethod
+    def calculate(equation):
+        # The equation provided will be split with (, ), and a space being the delimiters (without removing them)
+        equation = BirbLang.safesplit(equation, "(", ")", " ")
+        equation = BirbLang.removeall(equation, " ")
+        equation = BirbLang.removeall(equation, "")
+        result = 0
+        operations = ["floof", "unfloof", "megafloof", "megaunfloof", "ultrafloof", "ultraunfloof"]
+        # Changes the data type of any variable to its appropriate type
+        for x in equation:
+            if re.match("[0-9]+", x):
+                equation[equation.index(x)] = ast.literal_eval(x)
+
+        # Defines lambda functions which actually calculate the equation (based on the operation)
+        EXPONENT = lambda x: x[1] ** x[3]
+        MULTIPLICATION = lambda x: x[1] * x[3]
+        DIVISION = lambda x: x[1] / x[3]
+        ADDITION = lambda x: x[1] + x[3] if len(x) > 2 else x[1] + 1
+        SUBTRACTION = lambda x: x[1] - x[3] if len(x) > 2 else x[1] - 1
+        temp = [equation, []]
+        while True:
+            # Checks to see if there are any brackets within the equation, and will set temp[1] to the innermost
+            # brackets to be evaluated. Otherwise, it will just evaluate the equation as-is
+            if "(" in temp[0]:
+                evaluate = []
+                index = temp[0].index("(")
+                idunnoIllRenameLater = index
+                evaluate.append(temp[0][index])
+                index += 1
+
+                # Sets temp[1] to the first brackets detected within the equation
+                while BirbLang.count(evaluate, "(") != BirbLang.count(evaluate, ")"):
+                    evaluate.append(temp[0][index])
+                    index += 1
+                temp[1] += evaluate[1:-1]
+                evaluate = []
+                occurrence = 2
+
+                # Checks to see if there are any inner brackets, and will continue to shrink the array until it has
+                # reached the inner most equation
+                while "(" in temp[1]:
+                    index = temp[1].index("(") + 1
+                    evaluate.append("(")
+                    while BirbLang.count(evaluate, "(") != BirbLang.count(evaluate, ")"):
+                        evaluate.append(temp[1][index])
+                        index += 1
+                    temp[1] = evaluate[1:-1]
+                    idunnoIllRenameLater = BirbLang.indexofoccurrence(temp[0], "(", occurrence)
+                    occurrence += 1
+
+                # Removes the part of the equation that will be evaluated
+                del temp[0][idunnoIllRenameLater + 1:idunnoIllRenameLater + 6]
+            else:
+                # Sets temp[1] to the equation
+                idunnoIllRenameLater = 0
+                temp[1] += (temp[0])
+                del temp[0][1:]
+
+            # Checks if the operator being evaluated is an exponent
+            if operations[4] in temp[1]:
+                print(temp[0])
+                calculation = EXPONENT(temp[1][0:])  # Calculates it
+                result = calculation  # Sets the result variable to be the calculation (in case it's the final answer)
+                temp[1] = []
+                temp[0][idunnoIllRenameLater] = calculation  # Places the calculation within the equation
+                continue
+
+            # Calculates multiplication and division
+            elif operations[2] in temp[1] or operations[3] in temp[1]:
+                calculation = MULTIPLICATION(temp[1][0:]) if temp[1][0] == operations[2] else DIVISION(
+                    temp[1][0:])
+                result = calculation
+                temp[1] = []
+                temp[0][idunnoIllRenameLater] = calculation
+                continue
+
+            # Calculates addition and subtraction
+            elif operations[0] in temp[1] or operations[1] in temp[1]:
+                calculation = ADDITION(temp[1][0:]) if temp[1][0] == operations[0] else SUBTRACTION(
+                    temp[1][0:])
+                result = calculation
+                temp[1] = []
+                temp[0][idunnoIllRenameLater] = calculation
+                continue
+            break
+        return result
 
     def evaluate(self):
         variables = {}
         data_types = ("string", "int", "float", "boolean", "complex", "list")
         code = self.program.split("\n")  # Splits code into each individual line
+        operations = ["floof", "unfloof", "megafloof", "megaunfloof", "ultrafloof", "ultraunfloof"]
 
         # Checks to see if program can be executed and stopped successfully
         if "hatch egg" in code and "slep" in code:
@@ -28,7 +157,7 @@ class BirbLang:
                 if "#" in linesplit:
                     del linesplit[linesplit.index("#"):]
 
-                if not linesplit or not self.removeAll(linesplit, " "):
+                if not linesplit or not self.removeall(linesplit, " "):
                     line += 1
                     continue
 
@@ -43,7 +172,7 @@ class BirbLang:
                 linesplit = code[line].split(" ")
 
                 # Outputs element at specific index of array (if possible)
-                if re.match("^chirp\s.+\sat\s[0-9]+$", code[line]):
+                if re.match("^chirp .+ at [0-9]+$", code[line]):
                     if linesplit[1] in variables:
                         if type(variables[linesplit[1]]) is list:
                             print(variables[linesplit[1]][int(linesplit[3])])
@@ -57,34 +186,57 @@ class BirbLang:
                         raise NameError(f"{linesplit[1]} is not defined")
 
                 # Checks if the line follows the pattern "chirp (some variable)", and will then print out the text
-                elif re.match("^chirp\s.+$", code[line]):
+                elif re.match("^chirp .+$", code[line]):
 
-                    # Will try printing out the value if it were a valid data type
-                    try:
-                        print(ast.literal_eval(" ".join(linesplit[1:])))
+                    if linesplit[1] in operations:
+                        linesplit = BirbLang.safesplit(code[line], " ", "(", ")")
+                        linesplit = BirbLang.removeall(linesplit, " ")
+                        linesplit = BirbLang.removeall(linesplit, "")
+                        for x in linesplit[1:]:
+                            if x in variables:
+                                linesplit[linesplit.index(x)] = str(variables[x])
+                        print(BirbLang.calculate(" ".join(linesplit[1:])))
 
-                    # If it isn't a valid data type, it will try to output it as a variable
-                    except ValueError:
-                        if linesplit[1] in variables:
-                            print(variables[linesplit[1]][1:-1]) if type(
-                                variables[linesplit[1]]) == "str" else print(variables[linesplit[1]])
+                    else:
+                        # Will try printing out the value if it were a valid data type
+                        try:
+                            print(ast.literal_eval(" ".join(linesplit[1:])))
+
+                        # If it isn't a valid data type, it will try to output it as a variable
+                        except ValueError:
+                            if linesplit[1] in variables:
+                                print(variables[linesplit[1]][1:-1]) if type(
+                                    variables[linesplit[1]]) == "str" else print(variables[linesplit[1]])
+                            else:
+                                raise NameError(
+                                    f"{linesplit[1]} is not defined")  # Raises an error if all other cases failed
+
                         else:
-                            raise NameError(
-                                f"{linesplit[1]} is not defined")  # Raises an error if all other cases failed
+                            raise NameError(f"{linesplit[1]} is not defined")
 
                 # Runs if the line is meant to declare a new variable (new birb variable_name is value)
-                elif re.match("^new\sbirb\s[^\s]+\sis\s[^\s]+$", code[line]):
-                    try:
-                        # Converts string to its appropriate data type
-                        variables[linesplit[2]] = ast.literal_eval(linesplit[4])
-                    except ValueError:
-                        if linesplit[4] == "mimic":  # Handles case where the line asks for user input
-                            variables[linesplit[2]] = input(">>> ")
-                        else:
-                            raise ValueError(f"{variables[linesplit[2]]} is not a valid data type")
+                elif re.match("^new birb [^ ]+ is .+$", code[line]):
+                    if linesplit[4] in operations:
+                        linesplit = BirbLang.safesplit(code[line], " ", "(", ")")
+                        linesplit = BirbLang.removeall(linesplit, " ")
+                        linesplit = BirbLang.removeall(linesplit, "")
+                        for x in linesplit[4:]:
+                            if x in variables:
+                                linesplit[linesplit.index(x)] = str(variables[x])
+                        variables[linesplit[2]] = BirbLang.calculate(" ".join((linesplit[4:])))
+
+                    else:
+                        try:
+                            # Converts string to its appropriate data type
+                            variables[linesplit[2]] = ast.literal_eval(linesplit[4])
+                        except ValueError:
+                            if linesplit[4] == "mimic":  # Handles case where the line asks for user input
+                                variables[linesplit[2]] = input(">>> ")
+                            else:
+                                raise ValueError(f"{variables[linesplit[2]]} is not a valid data type")
 
                 # Checks if the line is "floof (variable)," and will increment it if possible
-                elif re.match("^floof\s[^\s]+$", code[line]):
+                elif re.match("^floof [^ ]+$", code[line]):
                     if linesplit[1] in variables:
                         if type(variables[linesplit[1]]) is int or type(variables[linesplit[1]]) is float:
                             variables[linesplit[1]] += 1
@@ -93,7 +245,7 @@ class BirbLang:
                     else:
                         raise NameError(f"{linesplit[1]} is not defined")
 
-                elif re.match("^unfloof\s[^\s]+$", code[line]):
+                elif re.match("^unfloof [^ ]+$", code[line]):
                     if linesplit[1] in variables:
                         if type(variables[linesplit[1]]) is int or type(variables[linesplit[1]]) is float:
                             variables[linesplit[1]] -= 1
@@ -103,7 +255,7 @@ class BirbLang:
                         raise NameError(f"{linesplit[1]} is not defined")
 
                 # Changes data type of variable if the line matches "(variable) is now (data_type)"
-                elif re.match("^breed\sof\s[^\s]+\sis\snow\s[^\s]+$", code[line]):
+                elif re.match("^breed of [^ ]+ is now [^ ]+$", code[line]):
                     if linesplit[2] in variables:
                         if linesplit[5] in data_types:
                             if linesplit[5] == "string":
@@ -136,7 +288,7 @@ class BirbLang:
                         raise NameError(f"{linesplit[2]} is not defined")
 
                 # Outputs the data type of a variables/value if the line matches "breed of (variable/value)"
-                elif re.match("^breed\sof\s[^\s]+$", code[line]):
+                elif re.match("^breed of [^ ]+$", code[line]):
                     try:
                         print(type(ast.literal_eval(linesplit[2])))
 
@@ -146,28 +298,44 @@ class BirbLang:
                         else:
                             raise NameError(f"{linesplit[2]} is not defined")
 
-                elif re.match("^[^\s]+\sat\s[0-9]+\sis\snow\s[^\s]+$", code[line]):
+                elif re.match("^[^ ]+ at [0-9]+ is now .+$", code[line]):
                     if linesplit[0] in variables:
-                        if type(ast.literal_eval(linesplit[5])) is type(variables[linesplit[0]][int(linesplit[2])]):
-                            variables[linesplit[0]][int(linesplit[2])] = ast.literal_eval(linesplit[5])
+                        if linesplit[5] in operations:
+                            linesplit = BirbLang.safesplit(code[line], " ", "(", ")")
+                            linesplit = BirbLang.removeall(linesplit, " ")
+                            linesplit = BirbLang.removeall(linesplit, "")
+                            for x in linesplit[5:]:
+                                if x in variables:
+                                    linesplit[linesplit.index(x)] = str(variables[x])
+                                elif x == "it":
+                                    linesplit[linesplit.index(x)] = str(variables[linesplit[0]][int(linesplit[2])])
+                            linesplit[5] = BirbLang.calculate(" ".join((linesplit[5:])))
+                            variables[linesplit[0]][int(linesplit[2])] = linesplit[5]
                         else:
-                            raise ValueError(
-                                f"{type(variables[linesplit[0]])} cannot be given value of type {type(ast.literal_eval(linesplit[3]))}")
+                            variables[linesplit[0]][int(linesplit[2])] = ast.literal_eval(linesplit[5])
                     else:
                         raise NameError(f"{linesplit[0]} is not defined")
 
                 # Changes value of variable (if possible) if the line matches "(variable) is now (value)"
-                elif re.match("^[^\s]+\sis\snow\s[^\s]+$", code[line]):
+                elif re.match("^[^ ]+ is now .+$", code[line]):
                     if linesplit[0] in variables:
-                        if type(ast.literal_eval(linesplit[3])) is type(variables[linesplit[0]]):
-                            variables[linesplit[0]] = ast.literal_eval(linesplit[3])
+                        if linesplit[3] in operations:
+                            linesplit = BirbLang.safesplit(code[line], " ", "(", ")")
+                            linesplit = BirbLang.removeall(linesplit, " ")
+                            linesplit = BirbLang.removeall(linesplit, "")
+                            for x in linesplit[3:]:
+                                if x in variables:
+                                    linesplit[linesplit.index(x)] = str(variables[x])
+                                elif x == "it":
+                                    linesplit[linesplit.index(x)] = str(variables[linesplit[0]])
+                            variables[linesplit[0]] = BirbLang.calculate(" ".join(linesplit[3:]))
+
                         else:
-                            raise ValueError(
-                                f"{type(variables[linesplit[0]])} cannot be given value of type {type(ast.literal_eval(linesplit[3]))}")
+                            variables[linesplit[0]] = ast.literal_eval(linesplit[3])
                     else:
                         raise NameError(f"{linesplit[0]} is not defined")
 
-                elif re.match("^peck\s[^\s]+\sat\s[0-9]+$", code[line]):
+                elif re.match("^peck [^ ]+ at [0-9]+$", code[line]):
                     if linesplit[1] in variables:
                         if type(variables[linesplit[1]]) is list:
                             del variables[linesplit[1]][int(linesplit[3])]
@@ -177,7 +345,7 @@ class BirbLang:
                         raise NameError(f"{variables[linesplit[1]]} is not defined")
 
                 # Removes the last element of an array if the line matches "peck (variable)"
-                elif re.match("^peck\s[^\s]+$", code[line]):
+                elif re.match("^peck [^ ]+$", code[line]):
                     if linesplit[1] in variables:
                         try:
                             variables[linesplit[1]].pop()
@@ -187,7 +355,7 @@ class BirbLang:
                         raise NameError(f"{linesplit[1]} is not defined")
 
                 # Inserts value into specific index of array
-                elif re.match("^feed\s[^\s]+\s.+\sat\s[0-9]+$", code[line]):
+                elif re.match("^feed [^ ]+ .+ at [0-9]+$", code[line]):
                     if linesplit[1] in variables:
                         if type(variables[linesplit[1]]) is list:
                             variables[linesplit[1]].insert(int(linesplit[-1]), ast.literal_eval(
@@ -198,7 +366,7 @@ class BirbLang:
                         raise NameError(f"{linesplit[1]} is not defined")
 
                 # Appends a value to an array
-                elif re.match("^feed\s[^\s]+\s.+$", code[line]):
+                elif re.match("^feed [^ ]+ .+$", code[line]):
                     if linesplit[1] in variables:
                         if type(variables[linesplit[1]]) is list:
                             variables[linesplit[1]].append(ast.literal_eval("".join(linesplit[2:])))
@@ -217,12 +385,9 @@ class BirbLang:
 
 BirbLang_Program = BirbLang("""
 hatch egg
-new birb borb is [5,4,2]
-feed borb 3 at 1
-chirp borb at 1
-borb at 1 is now 7
-chirp borb at 1
-chirp "hello world"
+new birb test is 5
+test is now floof it by 2
+chirp test
 slep
 """)
 BirbLang_Program.evaluate()
